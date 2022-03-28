45e9c73758aef682d3382bd0d45a664ff39bd6be
ntuoi3034 <ntuoi3034>
2022-03-28 11:59:33 +0700

    cập nhật

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
-
-$(document).ready(function(){
-    function checkUserId(){
-        let userID = $('#txtUserID').val();
-        let userIDRegex = /^[a-zA-Z0-9]{5,12}$/;
-        if(userIDRegex.test(userID)){
-            return true
-        }
-        return false
-    }
-
-$("#btnRegister").click(function(e){
-    e.preventDefault();
-    if(checkUserId()){
-        $("#statusOfUserID").text('UserID hợp lệ')
-        $("#statusOfUserID").css('color','blue')
+let userid = document.getElementById('user-id')
+let error_id=document.getElementById('error-id')
+userid.addEventListener('blur',function(){
+    var user_id = /^[a-z0-9_-]{5,12}$/
+    if(user-id.test(userid.value)){
+        error_id.textContent ="Hợp lệ"
+        error_id.style.color ='blue'
     }else{
-        $("#statusOfUserID").text('UserID không hợp lệ')
-        $("#statusOfUserID").css('color','red')
-    }
-    })
-})
+        error_id.textContent ="không Hợp lệ"
+        error_id.style.color ='red'
+      }
+ })
+
+ let password = document.getElementById('password')
+let error_PW=document.getElementById('errorPW')
+password.addEventListener('blur',function(){
+    var password1= /^[a-z0-9_-]{7,12}$/
+    if(password1.test(password.value)){
+        error_PW.textContent ="Hợp lệ"
+        error_PW.style.color ='blue'
+    }else{
+        error_PW.textContent ="không Hợp lệ"
+        error_PW.style.color ='red'
+      }
+ })
+
 let name_user = document.getElementById('name')
 let error_name = document.getElementById('error-name')
 name_user.addEventListener('blur', function(){
@@ -33,3 +37,6 @@ name_user.addEventListener('blur', function(){
         error_name.style.color = 'red'
     }
 })
+
+
+
